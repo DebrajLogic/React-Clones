61424957327bd86af4392df36b95a7a57afe17a2
Debraj <81313461+DebrajLogic@users.noreply.github.com>
2023-10-14 19:00:36 +0000

    Added the Header

---
+++ Gmail-Clone/src/components/Header/Header.jsx
@@ -0,0 +1,43 @@
+import "./Header.css";
+
+import MenuIcon from "@mui/icons-material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import AppsIcon from "@mui/icons-material/Apps";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+
+import { Avatar, IconButton } from "@mui/material";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <IconButton>
+          <MenuIcon />
+        </IconButton>
+        <img
+          src="https://www.vectorlogo.zone/logos/gmail/gmail-ar21.png"
+          alt=""
+        />
+      </div>
+
+      <div className="header__middle">
+        <SearchIcon />
+        <input type="text" placeholder="Search Email..." />
+        <ArrowDropDownIcon className="header__inputCaret" />
+      </div>
+
+      <div className="header__right">
+        <IconButton>
+          <AppsIcon />
+        </IconButton>
+        <IconButton>
+          <NotificationsIcon />
+        </IconButton>
+        <Avatar />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
